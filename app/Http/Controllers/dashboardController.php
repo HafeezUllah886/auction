297bb17e3394e8fb1cc9f13518a29adcdb41779a
HafeezUllah886 <Hafeezullah886@gmail.com>
2025-09-04 11:08:45 +0500

    dashboard

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\export_cars;
+use App\Models\export;
 use App\Models\purchase;
+use App\Models\OilPurchase;
+use App\Models\OilProducts;
 use Illuminate\Http\Request;
 
 class dashboardController extends Controller
@@ -15,8 +17,30 @@ class dashboardController extends Controller
 
         $purchases = purchase::whereBetween('date', [$from, $to]);
         $purchase_amount = $purchases->sum('total');
-        $purchase_count = $purchases->count();
+        $purchase_price_tax = $purchases->sum('ptax');
+        $purchase_auction_tax = $purchases->sum('atax');
+       
+        $export = export::whereBetween('date', [$from, $to]);
+        $export_amount = $export->sum('amount');
 
-        return view('dashboard.index', compact('purchase_amount', 'purchase_count'));
+        $oil_purchases = OilPurchase::whereBetween('date', [$from, $to]);
+        $oil_purchase_amount = $oil_purchases->sum('total');
+        $oil_tax = $oil_purchases->sum('tax_value');
+
+        $total_tax = $purchase_price_tax + $purchase_auction_tax +  $oil_tax;
+
+        $recycle = purchase::whereBetween('date', [$from, $to])->sum('recycle');
+
+        $products = OilProducts::all();
+
+        $oil_stock = 0;
+
+        foreach ($products as $product) {
+            $product->stock = getStock($product->id);
+            $oil_stock += $product->stock;
+        }
+
+
+        return view('dashboard.index', compact('purchase_amount', 'export_amount', 'oil_purchase_amount', 'from', 'to', 'oil_stock', 'total_tax', 'recycle'));
     }
 }
